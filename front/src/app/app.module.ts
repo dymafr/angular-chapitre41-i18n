6c69daed2614eee96bd1ed2924fa68a7052e13da
dymafr <contact@dyma.fr>
2022-09-15 18:49:22 +0200

    maj ng14

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,7 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+
+import { registerLocaleData } from '@angular/common';
 import localeFr from '@angular/common/locales/fr';
 
 registerLocaleData(localeFr);
